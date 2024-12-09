1b22a62be39489f466370a7e1ca4b0e17e3fc09c
mk880 <mk880@APEX>
2024-12-09 22:21:45 +0530

    react route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/*LOGO*/}
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image scr="logo.png" alt="logo" w={32} h={32} />
         <span>Blog</span>
-      </div>
+      </Link>
       {/*mobile menu*/}
       <div className="md:hidden">
         <div
@@ -24,29 +25,29 @@ function Navbar() {
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="/">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl text-white bg-blue-800">
               Login
             </button>
-          </a>
+          </Link>
         </div>
       </div>
 
       {/*Desktop menu*/}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/">
           <button className="py-2 px-4 rounded-3xl text-white bg-blue-800">
             Login
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
