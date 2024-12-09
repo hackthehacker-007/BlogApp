81b994a3412f02ad0f814ddfec74e5a5f6d21fe8
mk880 <mk880@APEX>
2024-12-09 12:16:37 +0530

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2lx:px-64">
+      <Navbar />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/*LOGO*/}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="./logo.png" className="w-8 h-8"></img>
+        <span>Blog</span>
+      </div>
+      {/*mobile menu*/}
+      <div className="md:hidden">
+        <div
+          className="text-4xl cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "≡"}
+        </div>
+        {/*mobile menu list*/}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 gap-8 font-medium text-lg transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl text-white bg-blue-800">
+              Login
+            </button>
+          </a>
+        </div>
+      </div>
+
+      {/*Desktop menu*/}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl text-white bg-blue-800">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
